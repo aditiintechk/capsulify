fd376d0578c70333f343f89d2dd3ea0e206b1149
madhuroshan <madhuroshan2102@gmail.com>
2025-05-01 17:00:53 +0530

    initialize nextjs app

---
+++ app/constants/index.ts
@@ -0,0 +1,47 @@
+export const CLOTHING_ITEMS = {
+  APPLE: {
+    tops: [],
+    bottoms: [],
+    dresses: [],
+    bags: [],
+    footwear: [],
+    layers: [],
+  },
+  PEAR: {},
+  HOURGLASS: {},
+  RECTANGLE: {},
+  INVERTED_TRIANGLE: {},
+};
+export const BODY_TYPES = [
+  {
+    name: "Apple",
+    description:
+      "Apple body type is characterized by a larger upper body and narrower hips.",
+    image: "/assets/images/bodytypes/apple.png",
+    clothingItems: CLOTHING_ITEMS.APPLE,
+  },
+  {
+    name: "Pear",
+    description:
+      "Pear body type is characterized by a smaller upper body and larger hips.",
+    image: "/assets/images/bodytypes/pear.png",
+  },
+  {
+    name: "Hourglass",
+    description:
+      "Hourglass body type is characterized by a balanced upper and lower body with a defined waist.",
+    image: "/assets/images/bodytypes/hourglass.png",
+  },
+  {
+    name: "Rectangle",
+    description:
+      "Rectangle body type is characterized by a straight silhouette with little definition at the waist.",
+    image: "/assets/images/bodytypes/rectangle.png",
+  },
+  {
+    name: "Inverted Triangle",
+    description:
+      "Inverted Triangle body type is characterized by a larger upper body and narrower hips.",
+    image: "/assets/images/bodytypes/inverted-triangle.png",
+  },
+];
