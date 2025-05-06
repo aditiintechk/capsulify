c60f582087b4d444bd29fe18a5cfc9b646a4ce48
madhuroshan <madhuroshan2102@gmail.com>
2025-05-06 14:27:24 +0530

    menubar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,31 +1,53 @@
-'use client'
-import { useRouter } from 'next/navigation'
+"use client";
+import { SignedIn, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
-	const router = useRouter()
+  const router = useRouter();
+  const { user } = useUser();
 
-	return (
-		<nav className='inventory-navigation-bar'>
-			<div className='inventory-navigation-content'>
-				<div
-					className='inventory-logo-container cursor-pointer'
-					onClick={() => router.push('/')}
-				>
-					<img
-						src='/assets/images/logo/logo.svg'
-						alt='Capsulify Logo'
-						className='app-logo'
-					/>
-				</div>
+  return (
+    <nav className="inventory-navigation-bar">
+      <div className="inventory-navigation-content">
+        <div
+          className="inventory-logo-container cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          <img
+            src="/assets/images/logo/logo.svg"
+            alt="Capsulify Logo"
+            className="app-logo"
+          />
+        </div>
 
-				<div className='inventory-profile-button'>
-					<button onClick={() => router.push('/profile')}>
-						Profile
-					</button>
-				</div>
-			</div>
-		</nav>
-	)
+        <button
+          onClick={() => router.push("/profile")}
+          className="cursor-pointer"
+        >
+          <SignedIn>
+            <div className="rounded-full">
+              {user?.imageUrl ? (
+                <Image
+                  src={user?.imageUrl}
+                  alt="Profile Image"
+                  width={32}
+                  height={32}
+                  className="rounded-full"
+                />
+              ) : (
+                <p className="w-full h-full font-bold cursor-pointer">
+                  {`${user?.firstName?.charAt(0).toUpperCase()}${user?.lastName
+                    ?.charAt(0)
+                    .toUpperCase()}`}
+                </p>
+              )}
+            </div>
+          </SignedIn>
+        </button>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- app/types/index.ts
+++ app/types/index.ts
@@ -4,3 +4,25 @@ export type CreateUserParams = {
   email: string;
   clerkId: string;
 };
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  clerkId: string;
+  bodyTypeId: number;
+  onboarded: number;
+};
+
+export type BodyType = {
+  name: string;
+  description: string;
+  image?: string;
+};
+
+export type InventoryItem = {
+  name: string;
+  filename: string;
+  category: string;
+};
