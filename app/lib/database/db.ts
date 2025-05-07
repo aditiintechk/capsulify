659196ed6f6b308688ba134ac123b7e7aa6bf507
Aditi <aditideshpande2908@gmail.com>
2025-05-07 15:56:42 +0530

    database changes

--- app/lib/database/db.ts
+++ app/lib/database/db.ts
@@ -2,10 +2,10 @@ import { Pool, PoolClient } from 'pg'
 
 export const connectToDatabase = async (): Promise<PoolClient> => {
 	const pool = new Pool({
-		host: process.env.DB_HOST || 'db.chceiitswdiczdpkihvo.supabase.co',
-		port: parseInt(process.env.DB_PORT || '5432'),
+		host: process.env.DB_HOST || 'aws-0-ap-southeast-1.pooler.supabase.com',
+		port: parseInt(process.env.DB_PORT || '6543'),
 		database: process.env.DB_NAME || 'postgres',
-		user: process.env.DB_USER || 'postgres',
+		user: process.env.DB_USER || 'postgres.chceiitswdiczdpkihvo',
 		password: process.env.DB_PASSWORD || '1qGygL29eeXkiGKRKKg6',
 		ssl: {
 			rejectUnauthorized: false, // Required for Supabase
